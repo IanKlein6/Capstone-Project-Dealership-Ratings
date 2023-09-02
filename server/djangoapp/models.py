3a611901bf6ca66f22430b8c5c4c68d69b221103
ianklein6 <ianklein66@gmail.com>
2023-09-02 07:27:30 -0400

    changes made from theia lab environment

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,14 +4,39 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# <HINT> Create a Car Make model `
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# <HINT> Create a Car Model model `
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    dealer_id = models.IntegerField()
+    
+    TYPE_CHOICES = [
+        ('SEDAN', 'Sedan'), 
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+    ]
+    type_car = models.CharField(max_length=50, choices=TYPE_CHOICES, default='SEDAN')
+        raise NotImplementedError)
+    year = models.DateField()
+
+    def __str__ (self):
+
+
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,9 +14,9 @@ from requests.auth import HTTPBasicAuth
 
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
-# def get_dealers_from_cf(url, **kwargs):
-# - Call get_request() with specified arguments
-# - Parse JSON results into a CarDealer object list
+def get_dealers_from_cf(url, **kwargs):
+- Call get_request() with specified arguments
+- Parse JSON results into a CarDealer object list
 
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
